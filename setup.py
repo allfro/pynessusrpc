4c3ee4341644a0290e2b1a9f306913fa67d39962
allfro <ndouba@gmail.com>
2012-10-09 22:46:07 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pynessusrpc',
+    author='Nadeem Douba',
+    version='0.1',
+    author_email='ndouba@gmail.com',
+    description='Python Nessus RPC client.',
+    license='GPL',
+    packages=find_packages('src'),
+    package_dir={ '' : 'src' },
+    zip_safe=False,
+    install_requires=[],
+    dependency_links=[]
+)
+
+
